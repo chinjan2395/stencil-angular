9eb37ce9c241c22c12f0961d2eff9097908c1618
Chinjan Patel <patel.chinjan531@gmail.com>
2022-04-17 19:44:57 +0530

    Temp changes

---
+++ projects/stencil-library/stencil.config.ts
@@ -0,0 +1,36 @@
+import { Config } from '@stencil/core';
+import { angularOutputTarget } from '@stencil/angular-output-target';
+import {ValueAccessorConfig} from '@stencil/angular-output-target/dist/types';
+
+const angularValueAccessorBindings: ValueAccessorConfig[] = [];
+
+export const config: Config = {
+  namespace: 'example-stencil-lib',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+    angularOutputTarget({
+      componentCorePackage: 'example-stencil-lib',
+      directivesProxyFile: '../../projects/component-library/src/lib/stencil-generated/components.ts',
+      directivesArrayFile: '../../projects/component-library/src/lib/stencil-generated/index.ts',
+      // valueAccessorConfigs: angularValueAccessorBindings,
+      // directivesProxyFile: '../angular-workspace/projects/component-library/src/lib/stencil-generated/components.ts',
+    }),
+    /*{
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },*/
+  ],
+};
